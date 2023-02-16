237f0e6a5e7a9bf88259471af70ee1246535d4b8
Mahbub khan <mahbubk@live.com>
2023-02-16 10:04:49 +0000

    Add project files.

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TasksApp.Data
+{
+    public class AppDBContext
+    {
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+
+            }
+
+            public DbSet<Task> TaskNames{ get; set; }
+        }
+    }
+}
---
+++ Models/TaskModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace TasksApp.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Task Table")]
+        public string Task{ get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Priotity { get; set; }
+        public DateTime? DateCreated { get; set; }
+
+        public DateTime? DeadLIne { get; set; }
+    }
+}
